6da53d0fa048279480f5de077e7e8df5daee6f74
EC2 Default User <ec2-user@ip-172-31-43-89.ap-northeast-1.compute.internal>
2023-09-05 11:51:49 +0000

    rspec完了

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2023_09_05_100114) do
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
