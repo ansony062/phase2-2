d65bd3b143f9b9813b1c8bdbd4672c2f64d3b0d1
EC2 Default User <ec2-user@ip-172-31-43-89.ap-northeast-1.compute.internal>
2023-09-05 20:07:37 +0000

    [ff]routes,views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'post_images/show'
   get 'post_comments/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -13,7 +15,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get "followings" => "reltionships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
