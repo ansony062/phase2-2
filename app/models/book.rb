758ed0ad77fc479faf24552ace1c023c97d01f75
EC2 Default User <ec2-user@ip-172-31-43-89.ap-northeast-1.compute.internal>
2023-09-10 03:29:10 +0000

    いいねsort機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
@@ -10,8 +11,8 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
-  
+
+
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
